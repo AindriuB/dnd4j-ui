e1285ad81df02ead889e390d4bcbf377c77a1c4c
AindriuB <aindriubannister@gmail.com>
2020-10-03 21:48:38 +0100

    filtering and expandable rows for description

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,8 +1,11 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { element } from 'protractor';
 import { ApiService} from '../api-service.service';
+import { Item } from '../model/item';
 
 
 
@@ -10,12 +13,22 @@ import { ApiService} from '../api-service.service';
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ItemsComponent implements OnInit {
   
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   
   displayedColumns: string[] = ['name', 'type', 'category', 'weight', 'cost'];
+  expandedElement: Item | null;
   dataSource;
   
   constructor(private apiService: ApiService) {
@@ -27,8 +40,14 @@ export class ItemsComponent implements OnInit {
       console.log(items);
       this.dataSource = new MatTableDataSource(items);
       this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
     });
     
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
